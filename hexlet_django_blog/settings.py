b1a395d1f359cd01fc5a963386bd5ee4bde8071d
NoFate35 <vsokol35@mail.ru>
2025-08-14 18:25:03 +0300

    Changes to be committed: 	new file:   article/__init__.py 	new file:   article/admin.py 	new file:   article/apps.py 	new file:   article/migrations/__init__.py 	new file:   article/models.py 	new file:   article/tests.py 	new file:   article/urls.py 	new file:   article/views.py 	modified:   settings.py 	modified:   urls.py

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'hexlet_django_blog',
+    'hexlet_django_blog.article',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
