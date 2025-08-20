c0ed8ee7b10049fe2ffd379e4253246a0c7fc5eb
NoFate35 <vsokol35@mail.ru>
2025-08-20 21:14:59 +0300

    Changes to be committed: 	modified:   hexlet_django_blog/article/templates/articles/index.html 	modified:   hexlet_django_blog/article/urls.py 	modified:   hexlet_django_blog/article/views.py
    	Lesson dynamic urls

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-
+from django.urls import reverse
 
 class IndexView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, tags, article_id):
         return render(
         request,
         "articles/index.html",
         context={
-            "app": "articles",
+            "tags": tags,
+            "article_id": article_id,
         },
     )
+
+def home_pageView(request):
+    return redirect (reverse('article', kwargs={'tags':'python', 'article_id':'42'}) )
