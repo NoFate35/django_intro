73afac52a9cf12e010b915d725d2b50930fa27b3
NoFate35 <vsokol35@mail.ru>
2025-08-22 16:06:38 +0300

    Changes to be committed: 	modified:   hexlet_django_blog/article/models.py

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class Article(models.Model):
     name = models.CharField(max_length=200)  # название статьи
     body = models.TextField()  # тело статьи
