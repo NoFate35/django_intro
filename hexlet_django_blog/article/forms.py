baec8a75682d9f67088398f344217ab7389871fa
NoFate35 <vsokol35@mail.ru>
2025-09-05 06:50:57 +0300

    Changes to be committed: 	modified:   hexlet_django_blog/article/forms.py

--- hexlet_django_blog/article/forms.py
+++ hexlet_django_blog/article/forms.py
@@ -5,3 +5,11 @@ class ArticleCommentForm(ModelForm):
     class Meta:
         model = ArticleComment
         fields = ["content"]
+
+
+#for hexlet
+
+class ArticleForm(ModelForm):
+	class Meta:
+		model = Article
+		fields = ["title", "content", "category"]
