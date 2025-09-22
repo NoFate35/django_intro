df13ca442ab69b3435a801b9526b3a47e6c4afb9
NoFate35 <vsokol35@mail.ru>
2025-09-22 15:40:37 +0300

    Changes to be committed: 	modified:   hexlet_django_blog/tests/tests_app.py

--- hexlet_django_blog/tests/tests_app.py
+++ hexlet_django_blog/tests/tests_app.py
@@ -1,16 +1,42 @@
-import factory
-import factory.random
-
-from simple_blog.articles.models import Article
-
-SEED = 4321
-
-factory.random.reseed_random(SEED)
+from django.test import TestCase
+from hexlet_django_blog.factories import ArticleFactory
 
 
-class ArticleFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Article
+class ArticleTest(TestCase):
+    def setUp(self):
+        self.article1 = ArticleFactory()
+        self.article2 = ArticleFactory()
 
-    title = factory.Faker("sentence", nb_words=5)
-    content = factory.Faker("paragraph", nb_sentences=5, variable_nb_sentences=False)
+    def test_create_article(self):
+        response = self.client.get("/articles/create/")
+        self.assertTemplateUsed(resptest:poetry run python3 manage.py test tests/onse, "articles/form.html")
+
+        data = {"title": "Test title", "content": "Test content"}
+        response = self.client.post("/articles/create/", data=data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get("/articles/")
+        self.assertContains(response, data["title"])
+
+    def test_update_article(self):
+        response = self.client.get(f"/articles/{self.article1.id}/update/")
+        self.assertTemplateUsed(response, "articles/form.html")
+        self.assertContains(response, self.article1.title)
+        self.assertContains(response, self.article1.content)
+
+        data = {"title": "Updated title", "content": "Updated content"}
+        response = self.client.post(f"/articles/{self.article1.id}/update/", data=data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get("/articles/")
+        self.assertContains(response, data["title"])
+
+    def test_delete_article(self):
+        response = self.client.get("/articles/")
+        self.assertContains(response, self.article2.title)
+
+        response = self.client.get(f"/articles/{self.article2.id}/delete/")
+        self.assertTemplateUsed(response, "articles/article_confirm_delete.html")
+
+        response = self.client.post(f"/articles/{self.article2.id}/delete/")
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get("/articles/")
+        self.assertNotContains(response, self.article2.title)
