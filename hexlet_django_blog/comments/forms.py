bb0a7239b742345be40d7760f3d3275d8388147b
NoFate35 <vsokol35@mail.ru>
2025-10-01 19:14:05 +0300

    Changes to be committed: 	modified:   hexlet_django_blog/comments/forms.py

--- hexlet_django_blog/comments/forms.py
+++ hexlet_django_blog/comments/forms.py
@@ -9,4 +9,12 @@ class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ["author", "text"]
+        widgets = {
+            "text": forms.Textarea(attrs={"rows": 3}),
+        }
+    def clean_text(self):
+        text = self.cleaned_data["text"]
+        if len(text.strip()) == 0:
+            raise forms.ValidationError("Comment text cannot be empty!")
+        return text
 # END
